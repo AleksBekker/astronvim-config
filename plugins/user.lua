0646df68ed4fcb417343ac39bfb2de33f74aff77
Aleks Bekker <alekso.bekker@gmail.com>
2023-08-12 00:34:25 -0400

    Add my configuration and remove provided comments

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,70 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
+  {
+    "max397574/better-escape.nvim",
+    opts = {
+      mapping = {"jk"}
+    }
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  },
+  -- LSP Config
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "isort",
+        "flake8",
+        "mypy",
+        "pyright",
+        "rust-analyzer",
+      }
+    }
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = { "rust" },
+  },
   -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
+  --   "saecki/crates.nvim",
+  --   ft = {"rust", "toml"},
+  --   config = function(_, opts)
+  --     local crates = require("crates");
+  --     crates.setup(opts);
+  --     crates.show()
+  --   end
   -- },
+
+  -- COLORS
+  {
+    "navarasu/onedark.nvim",
+    event = "VeryLazy",
+    opts = {
+      style="cool"
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      background = { light = "latte", dark = "frappe"},
+    },
+  },
+  {
+    "Mofiqul/dracula.nvim",
+    event = "VeryLazy",
+    opts = {
+      transparent_bg = true,
+    },
+  },
 }
